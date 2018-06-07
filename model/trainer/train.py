fb821445cead5a87375fef4fd6f1416ec36bad08
JoanRessing <hoiditbenik@live.nl>
2018-06-07 21:40:09 +0200

    batch generator should work online now

--- model/trainer/train.py
+++ model/trainer/train.py
@@ -1,88 +1,131 @@
 from __future__ import absolute_import
+
 import numpy as np
 import pandas as pd
-from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM
-from sklearn.model_selection import train_test_split
-from keras.preprocessing import sequence
+import tensorflow as tf
+
 import sklearn
 import argparse
 import cPickle
 import gzip  
 import json
+import random
+import os
+
 from tensorflow.python.lib.io import file_io
+from keras.models import Model
+from keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from keras.applications import VGG16
+
+try:
+	from batchgenerator import BatchGenerator, BatchSequence
+except:
+	from .batchgenerator import BatchGenerator, BatchSequence
 
 
 def load_data(path):
 
-    # Load images
-    
-    # Load and decompress training labels
-    with file_io.FileIO(path + 'data/y_train.pickle', mode='rb') as fp:
-        data = gzip.GzipFile(fileobj=fp)
-        y_train = cPickle.load(data)
-    
-    # Load and decompress validation labels
-    with file_io.FileIO(path + 'data/y_validation.pickle', mode='rb') as fp:
-        data = gzip.GzipFile(fileobj=fp)
-        y_validation = cPickle.load(data)
-    
-    return y_train, y_validation
+	# Load and decompress training labels
+	with file_io.FileIO(path + 'data/y_train.pickle', mode='rb') as fp:
+		data = gzip.GzipFile(fileobj=fp)
+		y_train = cPickle.load(data)
+	
+	# Load and decompress validation labels
+	with file_io.FileIO(path + 'data/y_validation.pickle', mode='rb') as fp:
+		data = gzip.GzipFile(fileobj=fp)
+		y_validation = cPickle.load(data)
+	
+	return y_train, y_validation
+	
 
-def preprocessing():
-    return None
+def preprocessing(dir):
+	return None
 
-def create_model():
-    model = Sequential()
-    model.add(Dense(42, activation='relu'))
-    model.add((Dense(6, activation='sigmoid')))
+def create_vgg_model():
+	# Init pre-trained network
+	base_model = VGG16(weights='imagenet', include_top=False, input_shape=(290,290,3))
 
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	# Adding the last two fully-connected layers
+	x = base_model.output
+	x = GlobalAveragePooling2D()(x) # global average pooling (flatten)
+	x = Dense(1024, activation='relu')(x) # should be rather large with 228 output labels
+	y = Dense(228, activation='softmax')(x) # sigmoid instead of softmax to have independent probabilities
 
-    model.summary()
+	model = Model(inputs=base_model.input, outputs=y)
+	
+	# Unfreeze last few layers
+	for layer in base_model.layers[:-4]:
+		layer.trainable = False
+	for layer in base_model.layers[-4:]:
+		layer.trainable = True
 
-    return model
+	# Use binary loss instead of categorical loss to penalize each output independently
+	model.compile(optimizer='adam', loss='binary_crossentropy')
+
+	return model
 
 
-def main(train_file, test_file, job_dir):
-    y_train, y_validation = load_data(train_file)
-    
-    # Save model weights
-    model.save('model.h5')
+def main(train_file, test_file, job_dir, n_epochs):
+	y_train, y_validation = load_data(train_file)
+	images_path_train = os.path.join(train_file, 'data/train/')
+	
+	training_gen = BatchGenerator(
+		input_dir=images_path_train,
+		y=y_train,
+		batch_size=32,
+		session = tf.Session(),
+		shuffle=False,
+		img_size=290
+	)
 
-    # Save model on google storage
-    with file_io.FileIO('model.h5', mode='r') as input_f:
-        with file_io.FileIO(job_dir + '/model.h5', mode='w+') as output_f:
-            output_f.write(input_f.read())
-    
-    print(y_train.shape,y_validation.shape)
+	# vgg = create_vgg_model()
+	
+	# for i in range(n_epochs):	
+		# for batch_x, batch_y in training_gen:
+			# vgg.fit(batch_x, batch_y)
+
+	# # Save model weights
+	# model.save('model.h5')
+
+	# # Save model on google storage
+	# with file_io.FileIO('model.h5', mode='r') as input_f:
+		# with file_io.FileIO(job_dir + '/model.h5', mode='w+') as output_f:
+			# output_f.write(input_f.read())
+			
+	print("main success")
 
 if __name__ == '__main__':
-    """
-    The argparser can also be extended to take --n-epochs or --batch-size arguments
-    """
-    parser = argparse.ArgumentParser()
-    
-    # Input Arguments
-    parser.add_argument(
-      '--train-file',
-      help='GCS or local paths to training data',
-      required=True
-    )
+	"""
+	The argparser can also be extended to take --n-epochs or --batch-size arguments
+	"""
+	parser = argparse.ArgumentParser()
+	
+	# Input Arguments
+	parser.add_argument(
+	  '--train-file',
+	  help='GCS or local paths to training data',
+	  required=True
+	)
 
-    parser.add_argument(
-      '--test-file',
-      help='GCS or local paths to test data',
-      required=False
-    )
+	parser.add_argument(
+	  '--test-file',
+	  help='GCS or local paths to test data',
+	  required=True
+	)
 
-    parser.add_argument(
-        '--job-dir',
-        help='GCS location to write checkpoints and export models',
-        required=True
-    )
-    args = parser.parse_args()
-    arguments = args.__dict__
-    print('args: {}'.format(arguments))
+	parser.add_argument(
+		'--job-dir',
+		help='GCS location to write checkpoints and export models',
+		required=True
+	)
 
-    main(args.train_file, args.test_file, args.job_dir)
+	parser.add_argument(
+		'--n-epochs',
+		help='Number of epochs to train the model for',
+		required=True
+	)
+	args = parser.parse_args()
+	arguments = args.__dict__
+	print('args: {}'.format(arguments))
+
+	main(args.train_file, args.test_file, args.job_dir, int(args.n_epochs))
