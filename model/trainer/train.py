5f578df0cc7f251297e64f75b2c66570862b590e
JoanRessing <hoiditbenik@live.nl>
2018-06-08 00:18:26 +0200

    now also trains vgg network (not yet using fit_generator)

--- model/trainer/train.py
+++ model/trainer/train.py
@@ -15,7 +15,7 @@ import os
 from tensorflow.python.lib.io import file_io
 from keras.models import Model
 from keras.layers import GlobalAveragePooling2D, Dense, Dropout
-from keras.applications import VGG16
+from keras.applications import VGG16, xception
 
 try:
 	from batchgenerator import BatchGenerator, BatchSequence
@@ -41,15 +41,12 @@ def load_data(path):
 def preprocessing(dir):
 	return None
 
-def create_vgg_model():
-	# Init pre-trained network
-	base_model = VGG16(weights='imagenet', include_top=False, input_shape=(290,290,3))
-
+def fine_tune_model(base_model):
 	# Adding the last two fully-connected layers
 	x = base_model.output
-	x = GlobalAveragePooling2D()(x) # global average pooling (flatten)
-	x = Dense(1024, activation='relu')(x) # should be rather large with 228 output labels
-	y = Dense(228, activation='softmax')(x) # sigmoid instead of softmax to have independent probabilities
+	x = GlobalAveragePooling2D()(x) 		# global average pooling (flatten)
+	x = Dense(1024, activation='relu')(x) 	# should be rather large with 228 output labels
+	y = Dense(228, activation='softmax')(x)	# sigmoid instead of softmax to have independent probabilities
 
 	model = Model(inputs=base_model.input, outputs=y)
 	
@@ -72,25 +69,40 @@ def main(train_file, test_file, job_dir, n_epochs):
 	training_gen = BatchGenerator(
 		input_dir=images_path_train,
 		y=y_train,
+		epochs=int(n_epochs),
 		batch_size=32,
 		session = tf.Session(),
 		shuffle=False,
 		img_size=290
 	)
 
-	# vgg = create_vgg_model()
+	# Initialize some pretrained keras model, add more models if want to stack/ensemble them
+	models = []
 	
-	# for i in range(n_epochs):	
-		# for batch_x, batch_y in training_gen:
-			# vgg.fit(batch_x, batch_y)
+	vgg_base = VGG16(weights='imagenet', include_top=False, input_shape=(290,290,3))
+	vgg = fine_tune_model(vgg_base)
+	models.append(vgg)
+	
+	# xception_base = Xception(weights='imagenet', include_top=False, input_shape=(290,290,3))
+	# xception = fine_tune_model(xception_base)
+	# models.append(xception)
+	
+	# Train all models
+	for model in models:
+		# Need to still define keras.utils.Sequence to use fit_generator
+		#model.fit_generator(training_gen)
+		
+		for i in range(n_epochs):
+			for batch_x, batch_y in training_gen:
+				model.fit(batch_x, batch_y[:,1:])
+	
+	# Save model weights
+	model.save('model.h5')
 
-	# # Save model weights
-	# model.save('model.h5')
-
-	# # Save model on google storage
-	# with file_io.FileIO('model.h5', mode='r') as input_f:
-		# with file_io.FileIO(job_dir + '/model.h5', mode='w+') as output_f:
-			# output_f.write(input_f.read())
+	# Save model on google storage
+	with file_io.FileIO('model.h5', mode='r') as input_f:
+		with file_io.FileIO(job_dir + '/model.h5', mode='w+') as output_f:
+			output_f.write(input_f.read())
 			
 	print("main success")
 
@@ -128,4 +140,4 @@ if __name__ == '__main__':
 	arguments = args.__dict__
 	print('args: {}'.format(arguments))
 
-	main(args.train_file, args.test_file, args.job_dir, int(args.n_epochs))
+	main(args.train_file, args.test_file, args.job_dir, args.n_epochs)
